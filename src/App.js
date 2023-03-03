431022346cb96f57c6ad6d384a94a283b01ad93e
Gleidson nascimento <ttt_akashi@hotmail.com>
2023-03-03 18:31:06 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Investing from './components/home';
+
+function App() {
+  return (
+    <div className="App">
+     <Investing />
+    </div>
+  );
+}
+
+export default App;
